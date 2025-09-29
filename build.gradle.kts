f4cd31b5c9d3b98cf75f8d9178ab6277f8436576
Megamiun <gabryelarm@gmail.com>
2025-09-29 12:14:58 -0300

    Starting project up!

---
+++ build.gradle.kts
@@ -0,0 +1,13 @@
+plugins {
+    java
+    id("org.springframework.boot") version "3.5.6"
+    id("io.spring.dependency-management") version "1.1.7"
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+}
